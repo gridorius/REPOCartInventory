f047d73d1bbec21f27e741154d7b68483b58b55b
gridorius <gridorius@yandex.ru>
2025-08-03 17:44:30 +0500

    hotfix

--- Patches/EnemyHealthPatch.cs
+++ Patches/EnemyHealthPatch.cs
@@ -10,7 +10,7 @@ public class EnemyHealthPatch
     [HarmonyPrefix]
     private static void Update(EnemyHealth __instance)
     {
-        if (!SemiFunc.IsMasterClientOrSingleplayer())
+        if (!SemiFunc.IsMasterClientOrSingleplayer() || !ModConfig.EnableImmortalEnemy.Value)
             return;
         var parent = __instance.GetComponentInParent<EnemyParent>();
         if (parent != null && LevelStats.ImmortalEnemies.Contains(parent))
